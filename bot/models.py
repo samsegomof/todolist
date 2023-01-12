5ea6f2cdffbeb9ac8ff51db580613049b7619e38
samsegomof <wemagonof91@gmail.com>
2023-01-13 03:18:25 +0500

    fix bot again

--- bot/models.py
+++ bot/models.py
@@ -4,16 +4,9 @@ from django.db import models
 from core.models import User
 
 
-# class TgUser(models.Model):
-#     tg_chat_id = models.BigIntegerField(verbose_name='ID чата')
-#     tg_user_id = models.BigIntegerField(verbose_name='ID пользователя')
-#     tg_username = models.CharField(max_length=32, validators=[MinLengthValidator(5)])
-#     user = models.ForeignKey('core.User', verbose_name='Пользователь', on_delete=models.PROTECT, null=True, blank=True)
-#     verification_code = models.CharField(max_length=10, null=True, blank=True, verbose_name='Верификационный код')
-
 class TgUser(models.Model):
-    chat_id = models.BigIntegerField(verbose_name='id чата')
-    user_ud = models.BigIntegerField(verbose_name='id пользователя')
+    chat_id = models.BigIntegerField(verbose_name='id чата', null=True)
+    user_ud = models.BigIntegerField(verbose_name='id пользователя', null=True)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.PROTECT, null=True, blank=True)
     verification_code = models.CharField(max_length=255, null=True, blank=True, verbose_name='Код верификации')
 
--- bot/serializers.py
+++ bot/serializers.py
@@ -4,17 +4,6 @@ from bot.models import TgUser
 from bot.tg import tg_client
 
 
-# class TgUserVerCodSerializer(serializers.ModelSerializer):
-    # class Meta:
-    #     model = TgUser
-    #     fields = "__all__"
-    #     read_only_fields = ("id", "tg_chat_id", "tg_user_id")
-    #
-    # def update(self, instance, validated_data):
-    #     instance = super().update(instance, validated_data)
-    #     tg_client.send_message(chat_id=instance.chat_id, text="Успешно")
-    #     return instance
-
 class TgUserVerCodSerializer(serializers.ModelSerializer):
     class Meta:
         model = TgUser
