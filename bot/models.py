bea6427e98a4859f579adec97a2330b92d6a8bdb
samsegomof <wemagonof91@gmail.com>
2023-01-13 14:07:50 +0500

    trying fix

--- bot/models.py
+++ bot/models.py
@@ -4,7 +4,7 @@ from core.models import User
 
 
 class TgUser(models.Model):
-    chat_id = models.BigIntegerField(verbose_name='id чата')
-    user_ud = models.BigIntegerField(verbose_name='id пользователя')
+    chat_id = models.BigIntegerField(verbose_name='id чата', null=True)
+    user_ud = models.BigIntegerField(verbose_name='id пользователя', null=True)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.PROTECT, null=True, blank=True)
     verification_code = models.CharField(max_length=255, null=True, blank=True, verbose_name='Код верификации')
