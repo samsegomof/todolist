1ce35678892c79dec550f13bb5e82901cefa54cc
samsegomof <wemagonof91@gmail.com>
2023-01-13 19:41:26 +0500

    add swagger in settings.py

--- todolist/settings.py
+++ todolist/settings.py
@@ -171,4 +171,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
